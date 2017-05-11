57faebb0705a3e6f35b1e4ce80ffe1120efb517a
Grenysherg <grenysherg@gmail.com>
2017-05-11 23:52:34 +0300

    Модальные окна

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,58 @@
 var writeCompanyLink = document.querySelector(".js-write-company-link");
 var writeCompanyModal = document.querySelector(".write-company");
-var close = writeCompanyModal.querySelector(".btn-close");
 var writeCompanyForm = writeCompanyModal.querySelector(".write-company-form");
-var name = writeCompanyModal.querySelector("[name=name]");
-var email = writeCompanyModal.querySelector("[name=email]");
-var letter = writeCompanyModal.querySelector("[name=letter]");
+var writeCompanyName = writeCompanyModal.querySelector("[name=name]");
+var writeCompanyEmail = writeCompanyModal.querySelector("[name=email]");
+var writeCompanyLetter = writeCompanyModal.querySelector("[name=letter]");
+var writeCompanyClose = writeCompanyModal.querySelector(".btn-close");
+
+var mapLink = document.querySelector(".company-location");
+var mapModal = document.querySelector(".map");
+var mapClose = mapModal.querySelector(".btn-close");
 
 writeCompanyLink.addEventListener("click", function(event) {
   event.preventDefault();
   writeCompanyModal.classList.add("modal-show");
+  writeCompanyName.focus();
 });
 
 writeCompanyForm.addEventListener("submit", function(event) {
-  if (!name.value) {
+  if (!writeCompanyName.value) {
     event.preventDefault();
-    console.log(name);
-    name.classList.add("input-error");
+    writeCompanyName.classList.add("input-error");
+  } else {
+    writeCompanyName.classList.remove("input-error");
+  }
+
+  if (!writeCompanyEmail.value) {
+    event.preventDefault();
+    writeCompanyEmail.classList.add("input-error");
+  } else {
+    writeCompanyEmail.classList.remove("input-error");
+  }
+
+  if (!writeCompanyLetter.value) {
+    event.preventDefault();
+    writeCompanyLetter.classList.add("input-error");
+  } else {
+    writeCompanyLetter.classList.remove("input-error");
   }
 });
 
-close.addEventListener("click", function(event) {
+writeCompanyClose.addEventListener("click", function(event) {
   event.preventDefault();
+  writeCompanyName.classList.remove("input-error");
+  writeCompanyEmail.classList.remove("input-error");
+  writeCompanyLetter.classList.remove("input-error");
   writeCompanyModal.classList.remove("modal-show");
+});
+
+mapLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapModal.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapModal.classList.remove("modal-show");
 });
